a326b8f971d4badc08ab0cd24d44ad58e6040280
Hamlethan27 <hamlethan1999@gmail.com>
2023-11-09 00:44:02 +0600

    created project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
 
 function App() {
+  useEffect(()=>{
+    let f = function(x:number) {alert(x)};
+    (function(){f(1)}())
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        something
+      </div>
     </div>
   );
 }
